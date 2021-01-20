656a35273e29fa5f78df4b0bbf698dfb06189915
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-20 18:47:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |brand|
-      if !BRANDS.include?(brand)
-        BRANDS << @brand        
-      end
+    if !BRANDS.include?(brand)
+      BRANDS << @brand        
     end
   end
 
